000ca7d3ac18d9279c32a102572979c24473fb1a
unixuser3 <78844597+unixuser3@users.noreply.github.com>
2023-12-24 18:19:23 +0530

    commit
    ocmmit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿  using System;
 
 internal class CalciumPlus
 {
@@ -8,6 +8,22 @@ internal class CalciumPlus
         Console.WriteLine("Welcome to Calcium+!");
         System.Threading.Thread.Sleep(2000); // Short delay for effect
         Console.Clear();
+
+        // Initialize num1 and num2 to 0
+        double num1 = 0, num2 = 0;
+        bool validInput = false;
+
+        // Get the first number
+        while (!validInput)
+        {
+            Console.Write("Enter the first number: ");
+            if (!double.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                continue;
+            }
+            validInput = true;
+        }
 
         // Get and validate operation
         char operation = ' '; // Initialize operation
@@ -27,18 +43,10 @@ internal class CalciumPlus
             validOperation = true;
         }
 
-        // Initialize num1 and num2 to 0
-        double num1 = 0, num2 = 0;
-        bool validInput = false;
+        // Get the second number
+        validInput = false; // Reset the validInput flag
         while (!validInput)
         {
-            Console.Write("Enter the first number: ");
-            if (!double.TryParse(Console.ReadLine(), out num1))
-            {
-                Console.WriteLine("Invalid input. Please enter a valid number.");
-                continue;
-            }
-
             Console.Write("Enter the second number: ");
             if (!double.TryParse(Console.ReadLine(), out num2))
             {
@@ -95,3 +103,6 @@ internal class CalciumPlus
         }
     }
 }
+```
+
+    This reorganized code first asks the user for the first number, then the operation, and finally the second number. The logical flow is more intuitive and follows the sequence you specified. Make sure to test the application to ensure that the changes work as expected!
